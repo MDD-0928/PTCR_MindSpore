2f928274999d12acbfdf22193debe01cb123d738
MDD-0928 <144899651+MDD-0928@users.noreply.github.com>
2023-09-25 12:10:21 +0800

    Delete src/model/mdl.py

--- src/model/mdl.py
+++
@@ -1,4 +0,0 @@
-import mindspore as ms
-import mindspore.nn as nn
-from PTCR_MindSpore.src.model.backbones import PTCR_MindSpore
-from PTCR_MindSpore.src.model.layers.gem_pool_MindSpore import GeneralizedMeanPoolingP as GeM
