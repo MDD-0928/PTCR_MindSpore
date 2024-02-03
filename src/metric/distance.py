d45b38ceca552bbfcb2483606680c9c48e185ec5
Qianxiang Meng <144899651+MDD-0928@users.noreply.github.com>
2024-02-03 23:41:04 +0800

    Update distance.py

--- src/metric/distance.py
+++ src/metric/distance.py
@@ -8,103 +8,215 @@
 #
 # Unless required by applicable law or agreed to in writing software
 # distributed under the License is distributed on an "AS IS" BASIS
-# WITHOUT WARRANT IES OR CONITTONS OF ANY KIND�?either express or implied.
+# WITHOUT WARRANT IES OR CONITTONS OF ANY KIND， either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ====================================================================================
 
-"""Compute feature distance."""
+"""Compute Rank."""
 
+from collections import defaultdict
 import numpy as np
 
 
-import mindspore
-from mindspore import Tensor
-import mindspore.ops as ops
-
-
-def compute_distance_matrix(input1, input2, metric='euclidean'):
-    """A wrapper function for computing distance matrix.
-
-    Args:
-        input1 (torch.Tensor): 2-D feature matrix.
-        input2 (torch.Tensor): 2-D feature matrix.
-        metric (str, optional): "euclidean" or "cosine".
-            Default is "euclidean".
-
-    Returns:
-        torch.Tensor: distance matrix.
+def eval_cuhk03(distmat, q_pids, g_pids, q_camids, g_camids, max_rank):
+    """Evaluation with cuhk03 metric
+    Key: one image for each gallery identity is randomly sampled for each query identity.
+    Random sampling is performed num_repeats times.
     """
-    # check input
-    assert isinstance(input1, Tensor)
-    assert isinstance(input2, Tensor)
-    assert input1.ndim == 2, 'Expected 2-D tensor, but got {}-D'.format(
-        input1.ndim
-    )
-    assert input2.ndim == 2, 'Expected 2-D tensor, but got {}-D'.format(
-        input2.ndim
-    )
-    assert input1.shape[1] == input2.shape[1]
+    num_repeats = 10
+    num_q, num_g = distmat.shape
 
-    if metric == 'euclidean':
-        distmat = euclidean_squared_distance(input1, input2)
-    elif metric == 'cosine':
-        distmat = cosine_distance(input1, input2)
-    else:
-        raise ValueError(
-            'Unknown distance metric: {}. '
-            'Please choose either "euclidean" or "cosine"'.format(metric)
+    if num_g < max_rank:
+        max_rank = num_g
+        print(
+            'Note: number of gallery samples is quite small, got {}'.
+            format(num_g)
         )
 
-    return distmat
+    indices = np.argsort(distmat, axis=1)
+    matches = (g_pids[indices] == q_pids[:, np.newaxis]).astype(np.int32)
+
+    # compute cmc curve for each query
+    all_cmc = []
+    all_ap = []
+    num_valid_q = 0.  # number of valid query
+
+    for q_idx in range(num_q):
+        # get query pid and camid
+        q_pid = q_pids[q_idx]
+        q_camid = q_camids[q_idx]
+
+        # remove gallery samples that have the same pid and camid with query
+        order = indices[q_idx]
+        remove = (g_pids[order] == q_pid) & (g_camids[order] == q_camid)
+        keep = np.invert(remove)
+
+        # compute cmc curve
+        raw_cmc = matches[q_idx][keep]
+        # binary vector, positions with value 1 are correct matches
+        if not np.any(raw_cmc):
+            # this condition is true when query identity does not appear in gallery
+            continue
+
+        kept_g_pids = g_pids[order][keep]
+        g_pids_dict = defaultdict(list)
+        for idx, pid in enumerate(kept_g_pids):
+            g_pids_dict[pid].append(idx)
+
+        cmc = 0.
+        for _ in range(num_repeats):
+            mask = np.zeros(len(raw_cmc), dtype=np.bool)
+            for _, idxs in g_pids_dict.items():
+                # randomly sample one image for each gallery person
+                rnd_idx = np.random.choice(idxs)
+                mask[rnd_idx] = True
+            masked_raw_cmc = raw_cmc[mask]
+            cmc_ = masked_raw_cmc.cumsum()
+            cmc_[cmc_ > 1] = 1
+            cmc += cmc_[:max_rank].astype(np.float32)
+
+        cmc /= num_repeats
+        all_cmc.append(cmc)
+        # compute AP
+        num_rel = raw_cmc.sum()
+        tmp_cmc = raw_cmc.cumsum()
+        tmp_cmc = [x / (i+1.) for i, x in enumerate(tmp_cmc)]
+        tmp_cmc = np.asarray(tmp_cmc) * raw_cmc
+        ap = tmp_cmc.sum() / num_rel
+        all_ap.append(ap)
+        num_valid_q += 1.
+
+    assert num_valid_q > 0, 'Error: all query identities do not appear in gallery'
+
+    all_cmc = np.asarray(all_cmc).astype(np.float32)
+    all_cmc = all_cmc.sum(0) / num_valid_q
+    mean_ap = np.mean(all_ap)
+
+    return all_cmc, mean_ap
 
 
-def euclidean_squared_distance(input1, input2):
-    """Computes euclidean squared distance.
-
-    Args:
-        input1 (torch.Tensor): 2-D feature matrix.
-        input2 (torch.Tensor): 2-D feature matrix.
-
-    Returns:
-        torch.Tensor: distance matrix.
+def eval_market1501(distmat, q_pids, g_pids, q_camids, g_camids, max_rank):
+    """Evaluation with market1501 metric
+    Key: for each query identity, its gallery images from the same camera view are discarded.
     """
-    m, n = input1.shape[0], input2.shape[0]
+    num_q, num_g = distmat.shape
 
-    shape_tensor1 = Tensor(np.zeros((m, n), dtype=np.float32))
-    shape_tensor2 = Tensor(np.zeros((n, m), dtype=np.float32))
-    op_pow = ops.Pow()
+    if num_g < max_rank:
+        max_rank = num_g
+        print(
+            'Note: number of gallery samples is quite small, got {}'.
+            format(num_g)
+        )
 
-    mat1 = op_pow(input1, 2).sum(
-        axis=1, keepdims=True).expand_as(shape_tensor1)
-    mat2 = op_pow(input2, 2).sum(
-        axis=1, keepdims=True).expand_as(shape_tensor2).T
-    distmat = mat1 + mat2
+    indices = np.argsort(distmat, axis=1)
 
+    matches = (g_pids[indices] == q_pids[:, np.newaxis]).astype(np.int32)
+
+    # compute cmc curve for each query
+    all_cmc = []
+    all_ap = []
+    all_inp = []
+    num_valid_q = 0.  # number of valid query
+
+    for q_idx in range(num_q):
+        # get query pid and camid
+        q_pid = q_pids[q_idx]
+        q_camid = q_camids[q_idx]
+
+        # remove gallery samples that have the same pid and camid with query
+
+        order = indices[q_idx]
+        remove = (g_pids[order] == q_pid) & (g_camids[order] == q_camid)
+        keep = np.invert(remove)
+        raw_cmc = matches[q_idx][keep]
     
-    matmul = ops.MatMul(False, True)
-    cast = ops.Cast()
-    input1 = cast(input1, mindspore.float16)
-    input2 = cast(input2, mindspore.float16)
-    output = cast(matmul(input1, input2), mindspore.float32)
-    distmat = distmat - 2 * output
+        # binary vector, positions with value 1 are correct matches
+        if not np.any(raw_cmc):
+            # this condition is true when query identity does not appear in gallery
+            continue
 
-    return distmat
+        cmc = raw_cmc.cumsum()
+
+        pos_idx = np.where(raw_cmc == 1)
+        max_pos_idx = np.max(pos_idx)
+        inp = cmc[max_pos_idx] / (max_pos_idx + 1.0)
+        all_inp.append(inp)
+
+        cmc[cmc > 1] = 1
+
+        all_cmc.append(cmc[:max_rank])
+        num_valid_q += 1.
+
+        # compute average precision
+        # reference: https://en.wikipedia.org/wiki/Evaluation_measures_(information_retrieval)#Average_precision
+        num_rel = raw_cmc.sum()
+        tmp_cmc = raw_cmc.cumsum()
+        tmp_cmc = [x / (i+1.) for i, x in enumerate(tmp_cmc)]
+        tmp_cmc = np.asarray(tmp_cmc) * raw_cmc
+        ap = tmp_cmc.sum() / num_rel
+        all_ap.append(ap)
+
+    assert num_valid_q > 0, 'Error: all query identities do not appear in gallery'
+
+    all_cmc = np.asarray(all_cmc).astype(np.float32)
+    all_cmc = all_cmc.sum(0) / num_valid_q
+    mean_ap = np.mean(all_ap)
+    mean_inp = np.mean(all_inp)
+
+    return all_cmc, mean_ap, mean_inp
 
 
-def cosine_distance(input1, input2):
-    """Computes cosine distance.
+def evaluate_py(
+        distmat,
+        q_pids,
+        g_pids,
+        q_camids,
+        g_camids,
+        max_rank,
+        use_metric_cuhk03
+):
+    '''choose evaluating method.'''
+    if use_metric_cuhk03:
+        return eval_cuhk03(
+            distmat, q_pids, g_pids, q_camids, g_camids, max_rank
+        )
+
+    return eval_market1501(
+        distmat, q_pids, g_pids, q_camids, g_camids, max_rank
+    )
+
+
+def evaluate_rank(
+        distmat,
+        q_pids,
+        g_pids,
+        q_camids,
+        g_camids,
+        max_rank=50,
+        use_metric_cuhk03=False,
+):
+    """Evaluates CMC rank.
 
     Args:
-        input1 (torch.Tensor): 2-D feature matrix.
-        input2 (torch.Tensor): 2-D feature matrix.
-
-    Returns:
-        torch.Tensor: distance matrix.
+        distmat (numpy.ndarray): distance matrix of shape (num_query, num_gallery).
+        q_pids (numpy.ndarray): 1-D array containing person identities
+            of each query instance.
+        g_pids (numpy.ndarray): 1-D array containing person identities
+            of each gallery instance.
+        q_camids (numpy.ndarray): 1-D array containing camera views under
+            which each query instance is captured.
+        g_camids (numpy.ndarray): 1-D array containing camera views under
+            which each gallery instance is captured.
+        max_rank (int, optional): maximum CMC rank to be computed. Default is 50.
+        use_metric_cuhk03 (bool, optional): use single-gallery-shot setting for cuhk03.
+            Default is False. This should be enabled when using cuhk03 classic split.
+        use_cython (bool, optional): use cython code for evaluation. Default is True.
+            This is highly recommended as the cython code can speed up the cmc computation
+            by more than 10x. This requires Cython to be installed.
     """
-    l2_normalize = ops.L2Normalize(axis=1)
-    input1_normed = l2_normalize(input1)
-    input2_normed = l2_normalize(input2)
-    matmul = ops.MatMul(False, True)
-    distmat = 1 - matmul(input1_normed, input2_normed)
-    return distmat
+
+    return evaluate_py(
+        distmat, q_pids, g_pids, q_camids, g_camids, max_rank,
+        use_metric_cuhk03
+    )
